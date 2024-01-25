d9e628635161e9a8423eaad58f2ab6520b8a16ab
Afiq <afiqk@bu.edu>
2024-01-25 09:18:56 -0500

    Created an app that has a button that when clicked, changes the text to "Hello World."

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val hello = findViewById<TextView>(R.id.helloworld)
+        button.setOnClickListener {
+            hello.setText(R.string.hello)
+        }
+    }
+}
